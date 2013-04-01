369feb72270b5b04c6898c64f7efd09cc3ccb24b
Stefan Kjartansson <esteban.supreme@gmail.com>
2013-04-01 00:39:46 +0000

    bit more testing

--- test.py
+++ test.py
@@ -19,15 +19,16 @@ class GeoTest(AsyncHTTPTestCase):
     def get_app(self):
         return Application([('/', GeoHandler)], debug=True)
 
-    def test_homepage(self):
+    def test_queries(self):
 
         q = '/?q=Laugavegur+12,101'
 
         self.http_client.fetch(self.get_url(q), self.stop)
         response = self.wait(timeout=30)
-        print(response.body)
-        self.assertIsNotNone(json.loads(response.body.decode('utf-8')))
-
+        j = json.loads(response.body.decode('utf-8'))
+        self.assertIsNotNone(j)
+        self.assertDictEqual(j['results'][0]['coordinates'],
+            {"lng": -21.9311738, "lat": 64.1460249})
 
 if __name__ == '__main__':
     unittest.main()
