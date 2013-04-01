99d0b2d1d45f83246a4e3d6f49806bbc897199f1
Stefan Kjartansson <esteban.supreme@gmail.com>
2013-04-01 00:39:30 +0000

    found a much simpler way to do the search lookup, just a single request to the geoserver

--- server.py
+++ server.py
@@ -1,79 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Quick geo-coding service for icelandic addresses
+"""
+from __future__ import unicode_literals, print_function, absolute_import
+
 import codecs
-import urllib.parse
 import json
+import urllib.parse
 
 import lxml.html
+from lxml import objectify
+from lxml.builder import E
+from lxml.etree import tostring
 
 from tornado import ioloop, gen, escape
-from tornado.web import asynchronous, RequestHandler, Application
 from tornado.httpclient import AsyncHTTPClient, HTTPRequest
+from tornado.web import asynchronous, RequestHandler, Application, HTTPError
 
 from geo import isnet93_to_wgs84
 
 
-base_url = 'http://www.skra.is/default.aspx'
-
-
+url = 'http://geo.skra.is/geoserver/wfs'
 postcodes = dict(i.split(';')[:2] for i in
     codecs.open('postnumer.txt', 'r',
     encoding='iso-8859-1').read().splitlines()[1:])
 
 
-def find_matches(body, place):
-    root = lxml.html.fromstring(body)
-    selector = '//table[@class="resulttable multifasteign"]//tbody/tr/.'
-
-    for tr in root.xpath(selector):
-        name = None
-
-        for td in tr:
-            header = td.get('header')
-            if not header:
-                continue
-
-            text = td.text_content().strip()
-            if header == 'heiti':
-                name = td.find('a').text_content().strip()
-            elif header == 'sveitarfelag':
-                if not text == place:
-                    break
-            elif header == 'landnumer':
-                yield (name, text)
-
-
-def build_geo_http_request(landnr):
-    url = 'http://geo.skra.is/geoserver/wfs'
+def build_geo_http_request(filters):
     params = {
         'service': 'wfs',
         'version': '1.1.0',
         'request': 'GetFeature',
         'typename': 'fasteignaskra:VSTADF',
         'outputformat': 'json',
-        'filter': '<Filter><PropertyIsLike wildCard="*" singleChar="#" escapeChar="!"><PropertyName>fasteignaskra:LANDNR</PropertyName><Literal>%s</Literal></PropertyIsLike></Filter>' % (landnr)
+        'filter': filters,
     }
-
     return HTTPRequest(url, method='POST',
         body=urllib.parse.urlencode(params))
 
 
-def build_search_http_request(street):
-    params = {
-        'pageid': 1000,
-        'selector': 'streetname',
-        'lsvfn': -1,
-        'streetname': street,
-        'submitbutton': 'Leita'
+def build_name(p):
+    name = p['HEITI_NF']
+    if p['HUSMERKING']:
+        name += ' %s' % p['HUSMERKING']
+    name += ', %d' % p['POSTNR']
+    return name
+
+
+def build_response_element(item):
+
+    p = item['properties']
+    return {
+        'display_name': build_name(p),
+        'street': p['HEITI_NF'],
+        'number': p['HUSMERKING'],
+        'postcode': p['POSTNR'],
+        'coordinates': isnet93_to_wgs84(*item['geometry']['coordinates']),
     }
 
-    return HTTPRequest(base_url +
-        '?%s' % urllib.parse.urlencode(params))
 
-
-def parse_geo_response(j):
-    for i in j['features']:
-        g = i.get('geometry')
-        if g and g.get('coordinates'):
-            return isnet93_to_wgs84(*g['coordinates'])
+def build_filter(street, number, postcode):
+    return tostring(E.Filter(
+        E.And(
+            E.PropertyIsEqualTo(
+                E.PropertyName("fasteignaskra:HEITI_NF"),
+                E.Literal(street)
+            ),
+            E.PropertyIsEqualTo(
+                E.PropertyName("fasteignaskra:HUSNR"),
+                E.Literal(number)
+            ),
+            E.PropertyIsEqualTo(
+                E.PropertyName("fasteignaskra:POSTNR"),
+                E.Literal(postcode)
+            ),
+    )))
 
 
 class GeoHandler(RequestHandler):
@@ -83,61 +85,34 @@ class GeoHandler(RequestHandler):
     def get(self):
         q = self.get_argument('q', None)
         if not q:
-            self.write('missing query')
-            self.finish()
-            return
+            raise HTTPError(400, "Missing query parameter")
 
         try:
             street, postcode = (i.strip() for i in q.strip().split(','))
+            street, number = street.split()
         except ValueError:
-            self.write('format is "search string,postcode", "Laugavegur 1, 101"')
-            self.finish()
-            return
+            raise HTTPError(400, "Unable to parse address")
 
         place = postcodes.get(postcode)
-
         if not place:
-            self.write('invalid postcode')
-            self.finish()
-            return
+            raise HTTPError(400, "Invalid postcode")
 
         http_client = AsyncHTTPClient()
-        request = build_search_http_request(street)
+        f = build_filter(street, number, postcode)
+
+        request = build_geo_http_request(f)
         response = yield gen.Task(http_client.fetch, request)
         response.rethrow()
 
-        # do the json by hand so we can stream the response
-        self.set_header("Content-Type", "application/json; charset=UTF-8")
-        self.write('{"response": [')
-        has_many = False
+        j = json.loads(response.body.decode('utf-8'))
 
-        for name, landnumer in find_matches(response.body, place):
-            request = build_geo_http_request(landnumer)
-            response = yield gen.Task(http_client.fetch, request)
-            response.rethrow()
-
-            # since we're doing the json by hand.
-            # gets toggled after the first element is written
-            if has_many:
-                self.write(',')
-
-            self.write(escape.json_encode(({
-                'name': name,
-                'postcode': int(postcode),
-                'coordinates': parse_geo_response(
-                    json.loads(response.body.decode('utf-8')))
-            })))
-
-            if not has_many:
-                has_many = True
-
-        self.write(']}')
+        self.write({'results': [build_response_element(i) for i in j['features']]})
         self.finish()
 
 
 application = Application([
     (r"/", GeoHandler),
-], debug=True)
+])
 
 
 if __name__ == "__main__":
